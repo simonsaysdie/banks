d1251ba1834ade0e77bc8cee3c7ef17daa5437da
Zura Guerra <guerrazuramx@gmail.com>
2016-04-08 13:53:16 -0500

    Close and open menu on mobile

--- web/static/js/materialize_select.js
+++ web/static/js/materialize_select.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
     $('select').material_select();
+
+    $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 240
+      edge: 'right', // Choose the horizontal origin
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    });
+      
 });
