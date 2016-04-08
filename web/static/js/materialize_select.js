e7680ccf85304f0c98b49ad7683a075c35d8a5e2
Zura Guerra <guerrazuramx@gmail.com>
2016-04-08 14:02:02 -0500

    Not working sidebar menu

--- web/static/js/materialize_select.js
+++ web/static/js/materialize_select.js
@@ -1,10 +1,17 @@
 $(document).ready(function() {
     $('select').material_select();
-
-    $('.button-collapse').sideNav({
-      menuWidth: 300, // Default is 240
-      edge: 'right', // Choose the horizontal origin
-      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-    });
       
 });
+
+
+$('.button-collapse').sideNav({
+  menuWidth: 300, // Default is 240
+  edge: 'right', // Choose the horizontal origin
+  closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+});
+
+/*
+$('.button-collapse').click(
+   $('.button-collapse').sideNav('show');
+);
+*/
